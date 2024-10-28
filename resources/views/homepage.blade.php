9bf82fd28443782d2c876a21d41ba5395e01b726
Hood <97504681+Hood@users.noreply.github.com>
2024-10-29 00:05:13 +0800

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="csstyles/homepage.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Notepad Homepage </h1>
+        <p id="npdesc">Whether jotting down spontaneous ideas, organizing to-do lists, or crafting the next big novel, the classic notepad remains an indispensable tool for creativity and productivity. Its crisp, blank pages offer endless possibilities, waiting eagerly to capture thoughts, sketches, or important reminders. Compact and portable, it's the perfect companion for any thinker, artist, or professional on the go. With its simple design and unassuming presence, the notepad invites a world of potential, making it more than just a collection of papers—it's a canvas for the mind.</p>
+        <a class="button" href="{{ route('notepads.view') }}">Click to go to NotePad</a><br><br>
+    </header>
+
+
+
+</body>
+
+<footer>
+    <small>&copy;ACT-2 LVCC Student AY2024-2025</small>
+</footer>
+</body>
+</html>
